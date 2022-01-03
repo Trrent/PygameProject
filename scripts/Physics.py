2d691fa2023c181cd84d0ba8c3cf6154bbfceb1b
BuildTools <unconfigured@null.spigotmc.org>
2022-01-03 23:27:58 +0500

    Add Point, updated Vector

--- scripts/Physics.py
+++ scripts/Physics.py
@@ -1,64 +1,98 @@
 from math import acos
 from Main import height
-from typing import Union
+from collections.abc import Iterable
+
+
+class Point:
+    def __init__(self, x: float, pg_y: float):
+        """
+        Инициализация точки в классической декартовой координатной плоскости (ось ординат направлена вверх)
+        :param x: абсцисса точки в плоскости pygame; float
+        :param pg_y: ордината точки в плоскости pygame; float
+        """
+        self.x, self.y = x, height - pg_y
+        self.pg_y = pg_y
+
+    def classic(self):
+        return self.x, self.y
+
+    def pygame(self):
+        return self.x, self.pg_y
+
+    def distance_to(self, other):
+        return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
 
 
 class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, vector_coords: Iterable[int, int] | Iterable[Point, Point]):
+        """
+        Представляет вектор в классической декартовой координатной плоскости.
+        Если isinstance(vector_coords, Iterable), то подразумевается исходная соотнесённость с классической декартовой
+        координатной плоскостью: vector{i, j}.
+        Иначе расчет: i = конец.x - начало.x, j = конец.y - начало.y
+        Iterable[0] = Point1 (начало)
+        Iterable[1] = Point2 (конец)
+        :param vector_coords:
+        """
+        vector_coords = list(vector_coords)
+        if isinstance(vector_coords[0], int):
+            self.i, self.j = vector_coords
+        else:
+            point1, point2 = vector_coords
+            self.i = point2.x - point1.x
+            self.j = point2.y - point1.y
 
     def get_coords(self):
-        return self.x, self.y
+        return self.i, self.j
 
     def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
+        return Vector((self.i + other.i, self.j + other.j))
 
     def __iadd__(self, other):
-        self.x += other.x
-        self.y += other.y
+        self.i += other.i
+        self.j += other.j
         return self
 
     def __sub__(self, other):
-        return Vector(self.x - other.x, self.y - other.y)
+        return Vector((self.i - other.i, self.j - other.j))
 
     def __isub__(self, other):
-        self.x -= other.x
-        self.y -= other.y
+        self.i -= other.i
+        self.j -= other.j
         return self
 
     def __mul__(self, other):
-        return Vector(self.x * other, self.y * other)
+        return Vector((self.i * other, self.j * other))
 
     def __rmul__(self, other):
-        return Vector(self.x * other, self.y * other)
+        return Vector((self.i * other, self.j * other))
 
     def __str__(self):
-        return f"Vector({round(self.x, 2)}, {round(self.y, 2)})"
+        return f"Vector({round(self.i, 2)}, {round(self.j, 2)})"
 
     def __repr__(self):
         return self.__str__()
 
     def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+        return self.i == other.i and self.j == other.j
 
     def __ne__(self, other):
-        return self.x != other.x or self.y != other.y
+        return self.i != other.i or self.j != other.j
 
     def length(self):
-        return (self.x ** 2 + self.y ** 2) ** 0.5
+        return (self.i ** 2 + self.j ** 2) ** 0.5
 
     def is_collinear(self, other):
-        return self.x / other.x == self.y / other.y
+        return self.i / other.i == self.j / other.j
 
     def is_aligned(self, other):
-        return Vector.is_collinear(self, other) and self.x / other.x > 0
+        return Vector.is_collinear(self, other) and self.i / other.i > 0
 
     def is_equal(self, other):
         return self.__eq__(other)
 
     def scalar_product(self, other):
-        return self.x * other.x + self.y * other.y
+        return self.i * other.i + self.j * other.j
 
     def is_orthogonal(self, other):
         return self.scalar_product(other) == 0
@@ -71,13 +105,12 @@ class Vector:
         return self.get_coords() == (0, 0)
 
     def normalize(self):
-        self.x /= self.length()
-        self.y /= self.length()
+        self.i /= self.length()
+        self.j /= self.length()
 
     def normalized(self):
-        return Vector(self.x / self.length(), self.y / self.length())
+        return Vector((self.i / self.length(), self.j / self.length()))
 
 
-def convert_coord(point: Union[int, Vector]):
-    """Конвертация координаты из одной плоскости в другую"""
-    return height - point if type(point) == int else Vector(point.x, height - point.y)
+def distance(point1: Point, point2: Point):
+    return point1.distance_to(point2)
