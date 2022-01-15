686b2b368feafc3ed03f730d99781f1b54b8cee7
Dinislam <dinislamsayfullin@gmail.com>
2022-01-15 13:07:10 +0500

    Add new buttons

---
+++ data/levels/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="terrain" tilewidth="50" tileheight="50" tilecount="1938" columns="38">
+ <image source="../sprites/Platforms/tileset.jpg" trans="3c3c3c" width="1920" height="2566"/>
+</tileset>
